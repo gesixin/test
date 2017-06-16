4736a62b36d82b95f5964e6e8f196b9b7062b04e
gesixin <gesixin@xin.com>
2017-06-16 16:35:39 +0800

    test2

---
+++ 1.php
@@ -0,0 +1,20 @@
+<?php 
+
+$arr = [
+    [
+        'a'=>1,
+        'b' => 3,
+    ],
+    [
+        'a' =>2,
+        'b' => 4,
+    ],
+    [
+        'a' => 3,
+        'b' =>4,
+    ]
+];
+
+$arr2 = array_column($arr, null,'a');
+
+var_dump('<pre>',$arr2);exit;
