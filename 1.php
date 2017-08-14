bd1f167cbccc89f95dd111dd9734e14d6fe0c614
gesixin <gesixin1986@163.com>
2017-08-14 10:50:26 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -9,6 +9,9 @@ $arr = [
         'a' =>2,
         'b' => 4,
     ],
+    
+    
+    
     [
         'a' => 3,
         'b' =>4,
@@ -16,4 +19,5 @@ $arr = [
 ];
 
 $arr2 = array_column($arr, null,'a');
+
 var_dump('<pre>',$arr2);exit;
