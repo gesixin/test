be84fc0980f660b704c1d45a3d83eb2494630d9c
gesixin <gesixin@xin.com>
2017-08-14 10:47:52 +0800

    tttt

--- 1.php
+++ 1.php
@@ -1,23 +1,20 @@
 <?php 
+$str = '';
 
-$arr = [
-    [
-        'a'=>1,
-        'b' => 3,
-    ],
-    [
-        'a' =>2,
-        'b' => 4,
-    ],
-    [
-        'a' => 3,
-        'b' =>4,
-    ]
-];
+$date_arr = getdate();
+if(!$date_arr['wday']) $date_arr['wday'] = 7;
 
-$arr2 = array_column($arr, null,'a');
+$thisMonday = mktime(0,0,1,$date_arr['mon'],$date_arr['mday'] - $date_arr['wday'] +1,$date_arr['year']);
+$lastMonday = mktime(0,0,1,$date_arr['mon'],$date_arr['mday'] - $date_arr['wday'] + 1 - 7,$date_arr['year']);
+$lastSunday = mktime(0,0,-1,$date_arr['mon'],$date_arr['mday'] - $date_arr['wday'] + 1,$date_arr['year']);
+$thisFirstDay = mktime(0,0,0,$date_arr['mon'],$date_arr['mday'] - $date_arr['mday'] + 1,$date_arr['year']);
+$lastMondayendDay = mktime(0,0,-1,$date_arr['mon'],$date_arr['mday'] - $date_arr['mday'] + 1,$date_arr['year']);
+$lastMondayFirstDay = mktime(0,0,0,$date_arr['mon'] - 1, 1,$date_arr['year']);
 
 
-
-
-var_dump('<pre>',$arr2);exit;
+echo date("Y-m-d H:i:s",$thisMonday),'--',$date_arr['mday'] - $date_arr['wday'],'<br>';
+echo date("Y-m-d H:i:s",$lastMonday),'--',$date_arr['mday'] - $date_arr['wday'],'<br>';
+echo date("Y-m-d H:i:s",$lastSunday),'--',$date_arr['mday'] - $date_arr['wday'],'<br>';
+echo date("Y-m-d H:i:s",$thisFirstDay),'--',$date_arr['mday'] - $date_arr['wday'],'<br>';
+echo date("Y-m-d H:i:s",$lastMondayendDay),'--',$date_arr['mday'] - $date_arr['wday'],'<br>';
+echo date("Y-m-d H:i:s",$lastMondayFirstDay),'--',$date_arr['mday'] - $date_arr['wday'],'<br>';
--- 2.php
+++ 2.php
@@ -1,30 +1,31 @@
 <?php 
 
-class test2{
-    
-    public function t1()
-    {
-        require '1.php';
-        $objT1 = new test1();
-        
-        $fn = function(){
-          $this->t2();  
-        };
-        
-        //$fn = test1::t1();
-        //$fn();
-        
-        $objT1->t3($fn);
-    }
-    
-    public function t2(){
-        
-        echo 345;
-    }
-    
+//$str = file_get_contents('http://localhost/test/1.php');
+//$str = file_get_contents('http://www.baidu.com/');
+
+echo '开始内存：'.$start = memory_get_usage(), '<br>';
+$str = '|水水水水|或|货|';
+$arr = [];
+for($i= 0;$i<5000;$i++)
+{
+    $arr[] = $str;
 }
+$arr2 = [];
+$str2 = implode('', $arr);
+echo '开始内存：'.$end = memory_get_usage(), '<br>';
 
-$t2 = new test2();
-$t2->t1();
+$arr3 = explode('|', $str);
 
+$count = preg_grep('|\|货1\||', $arr);
+$count2 = preg_grep('|\|或\||', $arr);
+$count3 = preg_grep('|\|水水水水\||', $arr);
+echo '开始内存：'.$end = memory_get_usage(), '<br>';
+echo convert($end - $start) ,'<br>','<br>';
+var_dump($count);exit;
 
+function convert($size)
+{
+    $unit=array('b','kb','mb','gb','tb','pb');
+    return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+}
+var_dump($arr,$arr2);
