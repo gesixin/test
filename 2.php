c9f91ace517f553cf6bc37f2a00fdf9450c85a5f
gesixin <gesixin@xin.com>
2018-03-22 16:42:50 +0800

    123

--- 2.php
+++ 2.php
@@ -1,30 +1,12 @@
-<?php 
+<?php
 
-class test2{
-    
-    public function t1()
-    {
-        require '1.php';
-        $objT1 = new test1();
-        
-        $fn = function(){
-          $this->t2();  
-        };
-        
-        //$fn = test1::t1();
-        //$fn();
-        
-        $objT1->t3($fn);
-    }
-    
-    public function t2(){
-        
-        echo 345;
-    }
-    
+
+
+
+$fn = function($a) use ($i) {
+    echo $a + $i;
+};
+
+for($i = 0; $i < 10;$i ++){
+    echo      $fn($i) ,'<br>';
 }
-
-$t2 = new test2();
-$t2->t1();
-
-
